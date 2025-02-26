60e097d754b67d6d574389afc089169adad624fb
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-02-26 14:50:01 -0500

    getInt done

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,7 +1,14 @@
-public class Main
+import java.util.Scanner;
+
+public class DevTest
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        String message = SafeInput.getNonZeroLenString(in, "Enter your message");
+        System.out.println("Your message is: " + message);
+
+        int number = SafeInput.getInt(in, "Enter a number");
+        System.out.println("The number you entered is: " + number);
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class SafeInput {
+public class SafeInput
+{
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
+
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int retInt = 0;
+        String trash = "";
+        boolean done = false;
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            if(pipe.hasNextInt())
+            {
+                retInt = pipe.nextInt();
+                done = true;
+            }
+            else
+            {
+                trash = pipe.nextLine();
+                System.out.println("You entered an invalid number: " + trash);
+            }
+            
+        }while(!done);
+
+        return retInt;
+    }
+
+
 }
